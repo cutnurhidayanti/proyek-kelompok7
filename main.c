119b5d9f159d7fc43e25f7c5f63fd60b4fce2b35
cutnurhidayanti <80440031+cutnurhidayanti@users.noreply.github.com>
2021-05-22 10:09:34 +0700

    menambahkan Signup

--- main.c
+++ main.c
@@ -4,52 +4,59 @@
 
 int main(int argc, char *argv[]){
 
-    if(argc != 3){
-        printf("anda gagal login\n");
+    char username[20], password[20];
+
+    if((argc==2)&&((strcmp(argv[1],"SignUp"))==0)){
+        
+        FILE *fw= fopen("login.bin","wb");
+            char login[30];
+            printf("====Daftarkan Akun Anda====\n");
+            printf("cara memasukkan usernamne dan password : 'username@password'\n");
+            printf("masukkan username dan password :");
+            scanf(" %[^\n]", login);
+
+            fwrite(login,sizeof(char),sizeof(login)/sizeof(char),fw);
+
+            fclose(fw);
+            printf("cara login : ./FileAplikasiProgramUtama username password");
+    }if((argc==2)&&((strcmp(argv[1],"SignUp")) != 0)){
+        printf("\nDaftarkan username dan password anda jika belum punya akun\n");
+        printf("cara daftar akun : ./FileAplikasiProgramUtama SignUp\n");
+    }
+
+            FILE *fr= fopen("login.bin", "rb");
+
+            if((fr)==NULL){
+                printf("gagal membuka file");
+                return EXIT_FAILURE;
+            }
+
+            char akun[20];
+            fread(akun, sizeof(char), sizeof(akun)/sizeof(char), fr);
+            
+            fclose(fr);
+
+            char *kalimat[3];
+            int i = 0;
+
+            kalimat[0] =strtok(akun, "@");
+            while(kalimat[i++] != NULL){
+                kalimat[i] = strtok(NULL, "@");
+            }
+
+            strcpy(username, kalimat[0]);
+            strcpy(password, kalimat[1]);
+
+    if((argc==3)&&((((strcmp(argv[1], username))==0))&&((strcmp(argv[2], password))==0))){//ketika berhasil login
+        printf("anda berhasil login\n");
+        printf("====selamat datang di game who want to be a milioner====\n");
+        printf("isi program kita disini ya gengs");
+    }if((argc==3)&&((((strcmp(argv[1], username)) != 0))||((strcmp(argv[2], password)) != 0))){// ga berhasil login
+        printf("anda gagal login, silahkan cek username dan password anda!\n");
         printf("cara login : ./FileAplikasiProgramUtama username password");
     }
 
-        FILE *fw= fopen("login.bin","wb");
-        char login[30]= "syakir@Code";
 
-        fwrite(login,sizeof(char),sizeof(login)/sizeof(char),fw);
-
-        fclose(fw);
-
-        char usernameinput[10], passwordinput[10];
-        strcpy(usernameinput, argv[1]);
-        strcpy(passwordinput, argv[2]);
-        char username[20], password[20];
-
-        FILE *fr= fopen("Database/login.bin", "rb");
-
-        if((fr)==NULL){
-            printf("gagal membuka file");
-            return EXIT_FAILURE;
-        }
-
-        char akun[20];
-        fread(akun, sizeof(char), sizeof(akun)/sizeof(char), fr);
-        
-        fclose(fr);
-
-        char *kalimat[3];
-        int i = 0;
-
-        kalimat[0] =strtok(akun, "@");
-        while(kalimat[i++] != NULL){
-            kalimat[i] = strtok(NULL, "@");
-        }
-
-        strcpy(username, kalimat[0]);
-        strcpy(password, kalimat[1]);
-
-    if((strcmp(usernameinput, username)==0)&&(strcmp(passwordinput, password)==0)){
-        printf("anda berhasil login\n");
-        printf("PROGRAM YANG GAME YANG MAU KITA BUAT YA GUYS");
-    }else{
-        printf("gagal login ke program, silahkah cek username dan password anda");
-    }
 
     return 0;
 }
